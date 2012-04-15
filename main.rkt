ac5eddbb0c6a0a76ac9d3a28294a9a09df460846
Thomas Chust <chust@web.de>
2012-04-15 17:34:08 +0200

    Added licensing information

--- main.rkt
+++ main.rkt
@@ -1,4 +1,21 @@
 #lang racket/base
+;; This file is part of Protocol Buffers for Racket.
+;; Copyright (c) 2012 by Thomas Chust <chust@web.de>
+;;
+;; Protocol Buffers for Racket is free software: you can redistribute
+;; it and/or modify it under the terms of the GNU Lesser General
+;; Public License as published by the Free Software Foundation, either
+;; version 3 of the License, or (at your option) any later version.
+;;
+;; Protocol Buffers for Racket is distributed in the hope that it will
+;; be useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE. See the GNU Lesser General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU Lesser General Public
+;; License along with Protocol Buffers for Racket. If not, see
+;; <http://www.gnu.org/licenses/>.
 (require
  "interface.rkt")
 
